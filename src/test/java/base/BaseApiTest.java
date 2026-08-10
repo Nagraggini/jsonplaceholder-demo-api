ee74453310f2bdb01b3e5ddd2625f09fb30f9c5f
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-08-10 20:18:41 +0200

    Create BaseApiTest.java

---
+++ src/test/java/base/BaseApiTest.java
@@ -0,0 +1,19 @@
+package base;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class BaseApiTest {
+	@BeforeEach
+	void setup() {
+		// Testing this website:
+		RestAssured.baseURI="https://jsonplaceholder.typicode.com/";
+		
+		RestAssured.requestSpecification=RestAssured
+				.given()
+				.accept(ContentType.JSON) // Response type is JSON
+				.contentType(ContentType.JSON); // Request body type is JSON
+	}
+}
