c00407afeb96edfa77649823fa33ae1ef473c713
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-08-16 09:35:22 +0200

    Create DeleteAlbumTest.java

---
+++ src/test/java/api/albums/deleteAlbum/DeleteAlbumTest.java
@@ -0,0 +1,14 @@
+package api.albums.deleteAlbum;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeleteAlbumTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
