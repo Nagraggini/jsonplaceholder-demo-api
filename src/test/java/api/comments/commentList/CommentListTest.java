2ee0c1cf0961f91a8a9c6a07e51a4f1c02000ace
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-08-17 10:42:11 +0200

    add two test for CommentListTest class

--- src/test/java/api/comments/commentList/CommentListTest.java
+++ src/test/java/api/comments/commentList/CommentListTest.java
@@ -1,16 +1,109 @@
 package api.comments.commentList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 
+import java.util.List;
+
+import org.apache.logging.log4j.core.config.status.StatusConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class CommentListTest {
+import base.BaseApiTest;
+import io.restassured.http.ContentType;
 
+class CommentListTest extends BaseApiTest{
+
+	/**
+	 * Task 101 – Retrieve All Comments
+
+	Endpoint:	
+	GET /comments
+	
+	Requirements:	
+	Send a GET request to /comments.
+	Verify that the status code is 200.
+	Verify that the response is a JSON array.
+	Verify that the response is not null.
+	Verify that the response is not empty.
+	Verify that the response contains exactly 500 comments.
+	Log the response only if validation fails.
+	 */
 	@Test
-	@DisplayName("Task 101 - ")
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Task 101 – Retrieve All Comments")
+	void retrieveAllCommentsTest() {
+		given()
+			.accept(ContentType.JSON) // Also include BaseApiTest class.
+		.when()
+			.get("/comments")
+		.then()
+			.log().ifValidationFails()
+			.statusCode(200)
+			.body("$", instanceOf(List.class)) // !
+			.body("$", notNullValue())
+			.body("$", not(empty()))	// !
+			.body("$.size()", equalTo(500));
 	}
+	
+	/**
+	 * Task 102 – Verify the Structure of Every Comment
+	
+	Endpoint:
+	
+	GET /comments
+	
+	Requirements:
+	
+	Send a GET request to /comments.
+	Verify that the status code is 200.
+	Verify that every comment contains all the following fields:
+	postId
+	id
+	name
+	email
+	body
+	Perform the field validation using one combined body assertion.
+	Log the response only if validation fails.
+	 */
+	@Test
+	@DisplayName("Task 102 – Verify the Structure of Every Comment")
+	void verifyTheStructureOfEveryCommentTest() {
+		given()
+		.when()
+			.get("/comments")
+		.then()
+			.log().ifValidationFails()
+			.statusCode(200)
+			.body("$", everyItem(
+					allOf( // !
+						hasKey("postId")
+						,hasKey("postId")
+						,hasKey("id")
+						,hasKey("name")
+						,hasKey("email")
+						,hasKey("body"))));
+	}
+	
+	/**
+	 * Task 103 – Verify That Required Fields Contain Valid Values
 
+	Endpoint:
+	
+	GET /comments
+	
+	Requirements:
+	
+	Send a GET request to /comments.
+	Verify that the status code is 200.
+	Verify that every postId is greater than zero.
+	Verify that every id is greater than zero.
+	Verify that every name is neither null nor empty.
+	Verify that every email is neither null nor empty.
+	Verify that every body is neither null nor empty.
+	Log the response only if validation fails.
+	 */
 }
