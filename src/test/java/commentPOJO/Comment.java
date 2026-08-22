14c7c2ee8462d175e07024eab31120f03faef5a5
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-08-22 13:01:25 +0200

    finished put http method on comments endpoint

--- src/test/java/commentPOJO/Comment.java
+++ src/test/java/commentPOJO/Comment.java
@@ -53,4 +53,12 @@ public class Comment {
 	public void setBody(String body) {
 		this.body = body;
 	}
+
+	@Override
+	public String toString() {
+		return "Comment [postId=" + postId + ", id=" + id + ", name=" + name + ", email=" + email + ", body=" + body
+				+ "]";
+	}
+	
+	
 }
